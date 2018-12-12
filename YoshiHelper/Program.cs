176bae3c8d5d68098af565edcd3ecbdcd05e9db0
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2018-12-12 12:16:31 +0100

    Beräknar och skriver ut hur lång tid det tar att gå till bussen

--- YoshiHelper/Program.cs
+++ YoshiHelper/Program.cs
@@ -13,19 +13,9 @@ namespace YoshiHelper
 
             WelcomeUser();
             List<Bus> busInfo = AskUserForDefaultSettings();
-            ReadTimetable();
+            //ReadTimetable();
             CalculateWalkingTime(busInfo);
             
-        }
-
-        private static void CalculateWalkingTime(List<Bus> busInfo)
-        {
-            double walkSpeed = 1.4;
-            var walktoStartStationSeconds = busInfo.Select(x => x.DistanceToStartStation / walkSpeed);
-            double walktoStartStationMinutes = Convert.ToDouble(walktoStartStationSeconds) / 60;
-
-            Console.WriteLine($"It takes {walktoStartStationMinutes:0.##} minutes to walk to the start station from home");
-
         }
 
         private static void WelcomeUser()
@@ -43,13 +33,13 @@ namespace YoshiHelper
             bus.StartStation = Console.ReadLine();
 
             Console.Write("Enter distance from home to start bus station (in m): ");
-            bus.DistanceToStartStation = int.Parse(Console.ReadLine());
+            bus.DistanceToStartStation = double.Parse(Console.ReadLine());
 
             Console.Write("Enter end bus station: ");
             bus.EndStation = Console.ReadLine();
 
             Console.Write("Enter distance from end bus station to work (in m): ");
-            bus.DistanceToEndStation = int.Parse(Console.ReadLine());
+            bus.DistanceToEndStation = double.Parse(Console.ReadLine());
 
             busInfo.Add(bus);
             return busInfo;
@@ -59,6 +49,28 @@ namespace YoshiHelper
         private static void ReadTimetable()
         {
             string[] timeTable = File.ReadAllLines(@"C:\Project\YoshiHelper\YoshiHelper\YoshiHelper\Gråbosnabben.txt");
+        }
+
+        private static void CalculateWalkingTime(List<Bus> busInfo)
+        {
+            double walkSpeed = 1.4;
+
+            var walktoStartStationSeconds = busInfo.Select(x => x.DistanceToStartStation / walkSpeed / 60);
+            Console.WriteLine($"It takes {string.Join(",",walktoStartStationSeconds):0.#} minutes to walk to the bus");
+
+            //double walktoStartStationMinutes = Convert.ToDouble(walktoStartStationSeconds) / 60;
+
+            //Console.WriteLine($"It takes {walktoStartStationMinutes:0.##} minutes to walk to the start station from home");
+
+            //Bus bus = new Bus();
+            //List<Bus> x = new List<Bus>();
+
+
+            //double walkToEndStationSeconds = bus.DistanceToEndStation / walkSpeed;
+            //double walkToEndStationMinutes = walkToEndStationSeconds / 60;
+            //Console.WriteLine($"It takes {walkToEndStationMinutes} minutes to walk to the end station from work");
+
+
         }
 
 
